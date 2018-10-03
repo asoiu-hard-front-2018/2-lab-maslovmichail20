b1bd29c2c186e71aa1ca1dad7d1b93457401cf11
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-10-03 20:49:07 +0300

    Add adaptive layout to the search-layer

--- src/search-button/search-button.js
+++ src/search-button/search-button.js
@@ -4,8 +4,11 @@ var searchButton = document.body.querySelector('.search-button');
 var pageWrapper = document.body.querySelector('.page__wrapper');
 var searchLayer = document.body.querySelector('.search-layer');
 
+
 function searchButtonOnClick() {
-    searchLayer.style.top = searchLayerIsActive ? '-20%' : '32.2%';
+    var layerTop = document.body.offsetWidth >= 990 ? '32.2%' : '25.5%';
+
+    searchLayer.style.top = searchLayerIsActive ? '-20%' : layerTop;
     searchLayer.style.opacity = searchLayerIsActive ? '0' : '1';
     pageWrapper.style.opacity = searchLayerIsActive ? '1' : '0.2';
 
